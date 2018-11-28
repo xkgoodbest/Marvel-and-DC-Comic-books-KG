bf2067b5e41c200615f590bc1ca9772714123346
Basel Shbita <basel921@gmail.com>
2018-11-27 17:38:55 -0800

    added some visualizations to the home page & use templates

--- webpages/test.py
+++ webpages/test.py
@@ -5,26 +5,7 @@ import random
 import json
 import sys
 
-app = Flask(__name__)
-app.secret_key = "super secret key"
-sparql = SPARQLWrapper("http://localhost:3030/mdccomics/query")
-sparql.setReturnFormat(JSON)
-
-@app.route('/')
-def output():
-    return render_template('index.html')
-
-@app.route('/query')
-def query_no_res():
-    return render_template('query.html', title='Query')
-
-@app.route('/query', methods=['POST'])
-def query():
-    # read the posted values from the UI
-    _sparql = request.form['sparql']
-    print(_sparql)
-    if _sparql:
-        sparql_q_pre = """
+SPARQL_PREFIXES = """
             prefix md: <http://www.w3.org/ns/md#>
             prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
             prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -38,7 +19,29 @@ def query():
             prefix mdcu: <http://inf558.org/comics#>
 
             """
-        sparql.setQuery(sparql_q_pre + _sparql)
+COLORS_LIST = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC"]
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+sparql = SPARQLWrapper("http://localhost:3030/mdccomics/query")
+sparql.setReturnFormat(JSON)
+
+@app.route('/')
+def output():
+    labels, values = get_publisher_labels_values()
+    colors = COLORS_LIST
+    return render_template('index.html', chart_viz=True, set=zip(values, labels, colors), values=values, labels=labels)
+
+@app.route('/query')
+def query_no_res():
+    return render_template('query.html', title='Query')
+
+@app.route('/query', methods=['POST'])
+def query():
+    # read the posted values from the UI
+    _sparql = request.form['sparql']
+    if _sparql:
+        sparql.setQuery(SPARQL_PREFIXES + _sparql)
         results = sparql.query().convert()
         keys=results["results"]["bindings"][0].keys()
         ret=list()
@@ -48,6 +51,30 @@ def query():
                 re.append(results["results"]["bindings"][i][k]['value'])
             ret.append(re)
         return render_template('query.html', title='Query', key=keys, result=ret)
+
+def get_publisher_labels_values():
+    _sparql = """
+            SELECT ?var (count(?subject) as ?count)
+            WHERE {
+              ?subject a mdcu:character ;
+                       dbp:publisher ?var .
+            }
+            GROUP BY ?var
+            ORDER BY DESC(?count)
+            """
+    sparql.setQuery(SPARQL_PREFIXES + _sparql)
+    results = sparql.query().convert()
+    keys = results["results"]["bindings"][0].keys()
+    int_results = results["results"]["bindings"]
+    labels, values = [], []
+    for i in range(len(int_results)):
+        for k in keys:
+            curr_val = int_results[i][k]['value']
+            if k == 'var':
+                labels.append(curr_val)
+            else:
+                values.append(curr_val)
+    return labels, values
 
 if __name__ == '__main__':
     app.run(debug=True)
