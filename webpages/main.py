b1a83a2e069948bf29886e86cccfc6900a7f4963
xkgoodbest <xkgoodbest@163.com>
2018-11-28 12:44:40 -0800

    get description data

--- webpages/main.py
+++ webpages/main.py
@@ -19,17 +19,19 @@ SPARQL_PREFIXES = """
             prefix mdcu: <http://inf558.org/comics#>
 
             """
-COLORS_LIST = ["#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA","#ABCDEF", "#DDDDDD", "#ABCABC"]
+COLORS_LIST = ["#F7464A", "#46BFBD", "#FDB45C",
+               "#FEDCBA", "#ABCDEF", "#DDDDDD", "#ABCABC"]
 CLASS_OPTS = ["mdcu:character", "mdcu:issue", "mdcu:movie"]
 # this is used for the default view
-CLASS_ATTR_BEST_OPT = {CLASS_OPTS[0]: 1, 
-                       CLASS_OPTS[1]: 1, 
+CLASS_ATTR_BEST_OPT = {CLASS_OPTS[0]: 1,
+                       CLASS_OPTS[1]: 1,
                        CLASS_OPTS[2]: 0}
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
 sparql = SPARQLWrapper("http://localhost:3030/mdccomics/query")
 sparql.setReturnFormat(JSON)
+
 
 @app.route('/')
 def output():
@@ -40,6 +42,7 @@ def output():
     cpredicate_clean = pred_opts[CLASS_ATTR_BEST_OPT[cclass]]
     return return_main_page_with_filters(cclass, cpredicate_clean, pred_opts)
 
+
 @app.route('/refreshfiltertop', methods=['GET'])
 def refresh_filter_top_results():
     # chosen_class has changed, need to re-generate the form
@@ -48,6 +51,7 @@ def refresh_filter_top_results():
     pred_opts = get_avilable_properties_for_class(cclass)
     cpredicate_clean = pred_opts[CLASS_ATTR_BEST_OPT[cclass]]
     return return_main_page_with_filters(cclass, cpredicate_clean, pred_opts)
+
 
 @app.route('/filtertop', methods=['GET'])
 def filter_top_results():
@@ -58,9 +62,11 @@ def filter_top_results():
     cpredicate_clean = request.args.get('chosen_attr')
     return return_main_page_with_filters(cclass, cpredicate_clean, pred_opts)
 
+
 @app.route('/query')
 def query_no_res():
     return render_template('query.html', title='Query')
+
 
 @app.route('/query', methods=['POST'])
 def query():
@@ -69,32 +75,97 @@ def query():
     if _sparql:
         sparql.setQuery(SPARQL_PREFIXES + _sparql)
         results = sparql.query().convert()
-        keys=results["results"]["bindings"][0].keys()
-        ret=list()
+        keys = results["results"]["bindings"][0].keys()
+        ret = list()
         for i in range(len(results["results"]["bindings"])):
-            re=list()
+            re = list()
             for k in keys:
-                if results["results"]["bindings"][i][k]['type']=='uri':
-                    re.append((results["results"]["bindings"][i][k]['value'],True))
+                if results["results"]["bindings"][i][k]['type'] == 'uri':
+                    re.append(
+                        (results["results"]["bindings"][i][k]['value'], True))
                 else:
-                    re.append((results["results"]["bindings"][i][k]['value'],False))
+                    re.append(
+                        (results["results"]["bindings"][i][k]['value'], False))
             ret.append(re)
         return render_template('query.html', title='Query', key=keys, result=ret)
 
 
 @app.route('/description', methods=['POST'])
 def description():
-    
-        return render_template('description.html')
+    data = request.get_json()
+    ret = get_all_attr(data)
+    keys = ['subject', 'predicate', 'object']
+    return render_template('description.html')
+
+
+def get_all_attr(data):
+    uri = '<' + data['uri'] + '>'
+    ret = list()
+    key1 = ['predicate', 'object']
+    _sparql1 = """
+            SELECT  ?predicate ?object
+            WHERE {
+              %s ?predicate ?object .
+            }
+            LIMIT 10
+            """ % (uri)
+    sparql.setQuery(SPARQL_PREFIXES + _sparql1)
+    results = sparql.query().convert()
+    if results["results"]["bindings"]:
+        for i in range(len(results["results"]["bindings"])):
+            re = list()
+            re.append(data['uri'])
+            for k in key1:
+                re.append(results["results"]["bindings"][i][k]['value'])
+            ret.append(re)
+
+    _sparql2 = """
+            SELECT  ?subject ?object
+            WHERE {
+              ?subject %s ?object .
+            }
+            LIMIT 10
+            """ % (uri)
+    sparql.setQuery(SPARQL_PREFIXES + _sparql2)
+    results = sparql.query().convert()
+    if results["results"]["bindings"]:
+        for i in range(len(results["results"]["bindings"])):
+            re = list()
+            re.append(data['uri'])
+            re.append(results["results"]["bindings"][i]['subject']['value'])
+            re.append(data['uri'])
+            re.append(results["results"]["bindings"][i]['object']['value'])
+            ret.append(re)
+    key3 = ['subject', 'predicate']
+    _sparql3 = """
+            SELECT  ?subject ?predicate
+            WHERE {
+              ?subject ?predicate %s .
+            }
+            LIMIT 10
+            """ % (uri)
+    sparql.setQuery(SPARQL_PREFIXES + _sparql3)
+    results = sparql.query().convert()
+    if results["results"]["bindings"]:
+        for i in range(len(results["results"]["bindings"])):
+            re = list()
+            for k in key3:
+                re.append(results["results"]["bindings"][i][k]['value'])
+            re.append(data['uri'])
+            ret.append(re)
+    return ret
+
 
 def return_main_page_with_filters(cclass, cpredicate_clean, pred_opts):
     cpredicate = '<' + cpredicate_clean + '>'
-    labels, values = get_top_labels_values_for_class_predicate(cclass, cpredicate)
+    labels, values = get_top_labels_values_for_class_predicate(
+        cclass, cpredicate)
     max_val = cast_and_find_max(values)
-    return render_template('index.html', chart_viz=True, 
-        classdropdown=CLASS_OPTS, attrdropdown=pred_opts,
-        selectedclass=cclass, selectedattr=cpredicate_clean,
-        set=zip(values, labels, COLORS_LIST), values=values, labels=labels, max_val=max_val)
+    return render_template('index.html', chart_viz=True,
+                           classdropdown=CLASS_OPTS, attrdropdown=pred_opts,
+                           selectedclass=cclass, selectedattr=cpredicate_clean,
+                           set=zip(values, labels, COLORS_LIST), values=values, labels=labels, max_val=max_val)
+
 
 def get_top_labels_values_for_class_predicate(class_uri, predicate_uri):
     _sparql = """
@@ -121,6 +192,7 @@ def get_top_labels_values_for_class_predicate(class_uri, predicate_uri):
                 values.append(curr_val)
     return labels, values
 
+
 def get_avilable_properties_for_class(class_uri):
     _sparql = """
             SELECT DISTINCT ?predicate
@@ -136,14 +208,15 @@ def get_avilable_properties_for_class(class_uri):
     results = sparql.query().convert()
     keys = results["results"]["bindings"][0].keys()
     int_results = results["results"]["bindings"]
-    
+
     predicates = []
     for i in range(len(int_results)):
         for k in keys:
             curr_val = int_results[i][k]['value']
             predicates.append(curr_val)
-    
+
     return predicates
+
 
 def cast_and_find_max(strlist):
     max_val = 0
@@ -154,6 +227,7 @@ def cast_and_find_max(strlist):
     return max_val
 
 # TODO: avoid error in case of no results...
+
 
 if __name__ == '__main__':
     app.run(debug=True)
